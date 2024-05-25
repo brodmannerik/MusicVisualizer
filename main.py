3290cab66da7a540dd4a074a96356439e9db2fdb
brodmannerik <s86299@bht-berlin.de>
2024-05-25 17:13:43 +0200

    not working well

--- main.py
+++ main.py
@@ -11,22 +11,8 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Audio Visualizer')
 
-#pyaudio init
-# CHUNK = 1024
-# FORMAT = pyaudio.paInt16
-# CHANNELS = 1
-# RATE = 44100
-#
-# p = pyaudio.PyAudio()
-# stream = p.open(format=FORMAT,
-#                 channels=CHANNELS,
-#                 rate=RATE,
-#                 input=True,
-#                 frames_per_buffer=CHUNK)
-
 p = pyaudio.PyAudio()
 
-# Load MP3 file
 mp3_file = "Toco_Meu_Rio.mp3"
 audio = AudioSegment.from_mp3(mp3_file)
 
@@ -36,14 +22,12 @@ format = p.get_format_from_width(audio.sample_width)
 channels = audio.channels
 rate = audio.frame_rate
 
-# Open PyAudio stream
-stream = p.open(format=format,
-                channels=channels,
-                rate=rate,
-                output=True)
-
 def play_audio():
-
+    # Open PyAudio stream
+    stream = p.open(format=format,
+                    channels=channels,
+                    rate=rate,
+                    output=True)
     # Play audio
     stream.write(pcm_data)
 
